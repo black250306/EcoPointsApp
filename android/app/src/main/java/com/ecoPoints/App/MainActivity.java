6e24124cd243dfa2d7345b104bf96e1d75f108ac
lamadridv44 <lamadridv44@gmail.com>
2025-11-17 00:08:42 +0000

    Añadir la nueva característica X y corregir bug Y

--- android/app/src/main/java/com/ecoPoints/App/MainActivity.java
+++ android/app/src/main/java/com/ecoPoints/App/MainActivity.java
@@ -1,11 +1,11 @@
 package com.ecoPoints.App;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
-
 import java.util.ArrayList;
 
-// Import all the plugins found in the project
+// Import the plugins we need to manually register
 import com.capacitorjs.plugins.camera.CameraPlugin;
 import com.capacitorjs.plugins.device.DevicePlugin;
 import com.capacitorjs.plugins.geolocation.GeolocationPlugin;
@@ -14,15 +14,15 @@ import com.capacitorjs.plugins.localnotifications.LocalNotificationsPlugin;
 import com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin;
 import com.capacitorjs.plugins.splashscreen.SplashScreenPlugin;
 import com.capacitorjs.plugins.statusbar.StatusBarPlugin;
-import com.capawesome.capacitor.androidedgetoedge.AndroidEdgeToEdgePlugin;
 
 public class MainActivity extends BridgeActivity {
 
   @Override
-  public ArrayList<Class<? extends Plugin>> getPlugins() {
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // The correct, modern way to manually register plugins
     ArrayList<Class<? extends Plugin>> plugins = new ArrayList<>();
-    // Add all the plugins you want to use here.
-    // This is the modern way to manually register plugins when auto-detection fails.
     plugins.add(CameraPlugin.class);
     plugins.add(DevicePlugin.class);
     plugins.add(GeolocationPlugin.class);
@@ -31,8 +31,7 @@ public class MainActivity extends BridgeActivity {
     plugins.add(PushNotificationsPlugin.class);
     plugins.add(SplashScreenPlugin.class);
     plugins.add(StatusBarPlugin.class);
-    plugins.add(AndroidEdgeToEdgePlugin.class);
-    return plugins;
-  }
 
+    registerPlugins(plugins);
+  }
 }
