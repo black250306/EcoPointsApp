e5d56804a509b7b5fe097e5a5584c9aa4b1a6779
lamadridv44 <lamadridv44@gmail.com>
2025-11-17 00:19:10 +0000

    Añadir la nueva característica X y corregir bug Y

--- android/app/src/main/java/com/ecoPoints/App/MainActivity.java
+++ android/app/src/main/java/com/ecoPoints/App/MainActivity.java
@@ -1,19 +1,10 @@
 package com.ecoPoints.App;
 
 import android.os.Bundle;
-import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import java.util.ArrayList;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
 
-// Import the plugins we need to manually register
-import com.capacitorjs.plugins.camera.CameraPlugin;
-import com.capacitorjs.plugins.device.DevicePlugin;
-import com.capacitorjs.plugins.geolocation.GeolocationPlugin;
-import com.capacitorjs.plugins.keyboard.KeyboardPlugin;
-import com.capacitorjs.plugins.localnotifications.LocalNotificationsPlugin;
-import com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin;
-import com.capacitorjs.plugins.splashscreen.SplashScreenPlugin;
-import com.capacitorjs.plugins.statusbar.StatusBarPlugin;
+import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
@@ -21,17 +12,14 @@ public class MainActivity extends BridgeActivity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    // The correct, modern way to manually register plugins
-    ArrayList<Class<? extends Plugin>> plugins = new ArrayList<>();
-    plugins.add(CameraPlugin.class);
-    plugins.add(DevicePlugin.class);
-    plugins.add(GeolocationPlugin.class);
-    plugins.add(KeyboardPlugin.class);
-    plugins.add(LocalNotificationsPlugin.class);
-    plugins.add(PushNotificationsPlugin.class);
-    plugins.add(SplashScreenPlugin.class);
-    plugins.add(StatusBarPlugin.class);
-
-    registerPlugins(plugins);
+    // Set a custom WebChromeClient to handle permission requests from the WebView
+    this.bridge.getWebView().setWebChromeClient(new WebChromeClient() {
+      @Override
+      public void onPermissionRequest(final PermissionRequest request) {
+        // Grant permission for video capture (camera).
+        // This is REQUIRED for JavaScript libraries like html5-qrcode to work inside a WebView.
+        request.grant(request.getResources());
+      }
+    });
   }
 }
