3aff828452e08ed41c3e0fa532e42ad1660dbeef
lamadridv44 <lamadridv44@gmail.com>
2025-11-17 00:02:08 +0000

    Añadir la nueva característica X y corregir bug Y

--- android/app/src/main/java/com/ecoPoints/App/MainActivity.java
+++ android/app/src/main/java/com/ecoPoints/App/MainActivity.java
@@ -1,5 +1,22 @@
 package com.ecoPoints.App;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
 
-public class MainActivity extends BridgeActivity {}
+import java.util.ArrayList;
+
+// Import the Camera plugin class
+import com.capacitorjs.plugins.camera.CameraPlugin;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // Manually add the Camera plugin here
+      add(CameraPlugin.class);
+    }});
+  }
+}
