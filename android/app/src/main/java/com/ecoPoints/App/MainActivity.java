abaa02e890f4a2dddc97652ef046a8263f06d4df
lamadridv44 <lamadridv44@gmail.com>
2025-11-17 00:05:27 +0000

    Añadir la nueva característica X y corregir bug Y

--- android/app/src/main/java/com/ecoPoints/App/MainActivity.java
+++ android/app/src/main/java/com/ecoPoints/App/MainActivity.java
@@ -1,22 +1,38 @@
 package com.ecoPoints.App;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
 
-// Import the Camera plugin class
+// Import all the plugins found in the project
 import com.capacitorjs.plugins.camera.CameraPlugin;
+import com.capacitorjs.plugins.device.DevicePlugin;
+import com.capacitorjs.plugins.geolocation.GeolocationPlugin;
+import com.capacitorjs.plugins.keyboard.KeyboardPlugin;
+import com.capacitorjs.plugins.localnotifications.LocalNotificationsPlugin;
+import com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin;
+import com.capacitorjs.plugins.splashscreen.SplashScreenPlugin;
+import com.capacitorjs.plugins.statusbar.StatusBarPlugin;
+import com.capawesome.capacitor.androidedgetoedge.AndroidEdgeToEdgePlugin;
 
 public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
 
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Manually add the Camera plugin here
-      add(CameraPlugin.class);
-    }});
+  @Override
+  public ArrayList<Class<? extends Plugin>> getPlugins() {
+    ArrayList<Class<? extends Plugin>> plugins = new ArrayList<>();
+    // Add all the plugins you want to use here.
+    // This is the modern way to manually register plugins when auto-detection fails.
+    plugins.add(CameraPlugin.class);
+    plugins.add(DevicePlugin.class);
+    plugins.add(GeolocationPlugin.class);
+    plugins.add(KeyboardPlugin.class);
+    plugins.add(LocalNotificationsPlugin.class);
+    plugins.add(PushNotificationsPlugin.class);
+    plugins.add(SplashScreenPlugin.class);
+    plugins.add(StatusBarPlugin.class);
+    plugins.add(AndroidEdgeToEdgePlugin.class);
+    return plugins;
   }
+
 }
